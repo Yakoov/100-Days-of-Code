8443e4cb7ef26da38a51287856d1864aa0349182
Jeremiah Jacob <jeremiahjacob000@gmail.com>
2022-07-25 13:04:24 +0100

    100 Python Programming- Detailed Readme.MD

--- Day 5/7 Final Project.py
+++ Day 5/7 Final Project.py
@@ -11,7 +11,43 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = ""
+# assume character = 4
+# We added plus one to pick the last value
+for character in range(1,nr_letters + 1):
+    password += random.choice(letters)
 
+for character in range(1, nr_symbols +1):
+    password += random.choice(symbols)
+
+for character in range(1,nr_numbers + 1):
+    password += random.choice(numbers)
+
+print(password)
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+# add to a list and not a string
+# use the append function to add items to a list
+password_list = []
+for character in range(1,nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for character in range(1, nr_symbols +1):
+    password_list.append (random.choice(symbols))
+
+for character in range(1,nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+# using the "shuffle()" function to reorder the list
+random.shuffle(password_list)
+
+# let's convert the list to a string using for loops
+
+password = ""
+for character in password_list:
+    password += character
+
+print(f"Your password is {password}")
+
+print(password_list)
--- README.md
+++ README.md
@@ -8,3 +8,19 @@ This Repository is created from the 100 Days of Coding with python by Dr. Angela
 Whose course i am following for my python Journey.
 
 The Basic Explanation and Detailed steps can help a Begineer to Start out with Python language.
+
+Day 1
+This is just a brief introudction to python, talking about the syntax and the sematics of the language, Covered the Strings, input and variables.
+To wrap it all up, There is a project at the end to apply this knowledge given. The project is about generating a bandname generator for the a user
+
+Day 2
+In day 2 the topics covered where on the datatypes in python and how to manipulate around such DataType.
+The Mathematical Operators was also convered with some coding exercise to help practice and understand the datatypes and mathematical operators.
+Finally, A project to wrap up to check the understanding of the concept.
+
+Day 3
+Day 3 taking a deep dive into the python, Topics Covered were conditional statements nested conditional statement and the logical operators.
+Fun exercise were solved to check understanding of the concept.
+Ending with a Project which uses what we have been taught to create and adventure game
+
+Day 4
